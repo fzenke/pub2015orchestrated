a44b2a4f3862e5dfd217f6ba063c0de183ef20c2
Friedemann Zenke <fzenke@stanford.edu>
2015-12-01 16:48:34 -0800

    Fixes bug in 3run_cued
    The path to shapes_cues.pat was wrong

--- 2015orchestrated/3run_cued.sh
+++ 2015orchestrated/3run_cued.sh
@@ -7,7 +7,7 @@ make -C $DIR -j8 $BIN && mpirun -n 4 $DIR/$BIN \
 	--load $OUTDIR/rf2 \
 	--prefix rf3 --size 4096 --save \
 	--monf ./data/rf1.pat \
-	--stim ./shapes_cues.pat \
+	--stim ./data/shapes_cues.pat \
 	--wie 0.15 --wee 0.1 --wext 0.1 \
 	--simtime 172800 --tauf 0.6 \
 	--extsparse 0.05 \
