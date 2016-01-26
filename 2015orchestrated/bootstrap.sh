e1991ed0e7572ee43ab39e9c0f57cfad0035cab1
Friedemann Zenke <fzenke@stanford.edu>
2016-01-25 17:03:22 -0800

    Adds cli output and workaround for bug in rassta.py

--- 2015orchestrated/bootstrap.sh
+++ 2015orchestrated/bootstrap.sh
@@ -3,23 +3,26 @@
 # Regenerates the monitor file "rf1.pat" which 
 # stores the four different selective subpopulations.
 
-# Adjust this variable to the output path of your network simulation
-DIR="$HOME/tmp/sim"
-
+# Load global vars such as OUTDIR
+. ./globalvars.sh
 
 # Leave unchanged in most cases
 T_START=3000
 T_STOP=3500
 
-# Create trigger file for PSTH computation
-./scripts/mktrigfile.awk $DIR/rf1.0.stimtimes > data/rf1.trig
+echo "Running first leg of simulation to determine neuron selectivity."
+./1run_init.sh
 
-# Create human-readable ras file from binary output files 
+echo "Generating trigger file"
+./scripts/mktrigfile.awk $OUTDIR/rf1.0.stimtimes > data/rf1.trig
+
+echo "Create human-readable ras file from binary output files "
 # (Assumes the auryn tool aube is accessible within your PATH)
-aube --input $DIR/rf1.*.e.bras --from $T_START --to $T_STOP > $DIR/rf1.e.ras
+aube --input $OUTDIR/rf1.*.e.bras --from $T_START > $OUTDIR/rf1.e.ras
 
-# Creates PSTH using our trigger file
-scripts/rassta.py -t data/rf1.trig -w 0.5 -f $DIR/rf1.e.ras -s $T_START -m $T_STOP -q 0.0 -o data/rf1.sta
+echo "Computing PSTH..."
+scripts/rassta.py -t data/rf1.trig -w 0.5 -f $OUTDIR/rf1.e.ras -s $T_START -m $T_STOP -q 0.0 -o data/rf1.sta
 
-# Generate pattern file through threshold computation
+echo "Generating pattern file..."
 scripts/sta2pat.sh data/rf1.sta data/rf1.pat
+echo "done"
