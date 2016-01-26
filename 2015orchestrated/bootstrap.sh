a1e714a8b1ff2426d2ed60ff9f5cddbffc2be19a
Friedemann Zenke <fzenke@stanford.edu>
2016-01-25 19:57:33 -0800

    Changes bootstrap procedure

--- 2015orchestrated/bootstrap.sh
+++ 2015orchestrated/bootstrap.sh
@@ -18,10 +18,10 @@ echo "Generating trigger file"
 
 echo "Create human-readable ras file from binary output files "
 # (Assumes the auryn tool aube is accessible within your PATH)
-aube --input $OUTDIR/rf1.*.e.bras --from $T_START > $OUTDIR/rf1.e.ras
+aube --input $OUTDIR/rf1.*.e.bras --from $T_START > $OUTDIR/rf1.ras
 
 echo "Computing PSTH..."
-scripts/rassta.py -t data/rf1.trig -w 0.5 -f $OUTDIR/rf1.e.ras -s $T_START -m $T_STOP -q 0.0 -o data/rf1.sta
+scripts/rassta.py -t data/rf1.trig -w 0.5 -f $OUTDIR/rf1.ras -s $T_START -m $T_STOP -q 0.0 -o data/rf1.sta
 
 echo "Generating pattern file..."
 scripts/sta2pat.sh data/rf1.sta data/rf1.pat
--- 2015orchestrated/run_orchestrated_stdp.sh
+++ 2015orchestrated/run_orchestrated_stdp.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-./1run_init.sh && ./2run_learn.sh && ./3run_cued.sh
+./bootstrap.sh && \
+	./1run_init.sh && \
+	./2run_learn.sh && \
+	./3run_cued.sh
