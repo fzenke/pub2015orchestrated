74a62699a0dc1211d80b48492c3e261bdcb6f55c
Friedemann Zenke <fzenke@stanford.edu>
2016-01-26 19:10:36 -0800

    Shortens default simtime for 3rd stage to 1h

--- 2015orchestrated/3run_cued.sh
+++ 2015orchestrated/3run_cued.sh
@@ -9,7 +9,7 @@ make -C $DIR -j8 $BIN && mpirun -n 4 $DIR/$BIN \
 	--monf ./data/rf1.pat \
 	--stim ./data/shapes_cues.pat \
 	--wie 0.15 --wee 0.1 --wext 0.1 \
-	--simtime 172800 --tauf 0.6 \
+	--simtime 3600 --tauf 0.6 \
 	--extsparse 0.05 \
 	--off 20.0 --on 0.2 \
 	--beta $BETA --eta $ETA --bgrate $BGRATE --scale $SCALE --weight_a $WEIGHTA --alpha $ALPHA --delta 0.02
