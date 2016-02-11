d6901098c7cb569cc7d70057affd281dcc26ad70
Friedemann Zenke <fzenke@stanford.edu>
2016-02-11 15:39:00 -0800

    Fixes problem in bootstrap.sh
    This fixes a previously introduced problem in bootstrap.sh which was
    erroneously importing functionality from the develop branch.
    This fixes #1.

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -16,9 +16,8 @@ echo "Running first leg of simulation to determine neuron selectivity."
 echo "Generating trigger file"
 ./scripts/mktrigfile.awk $OUTDIR/rf1.0.stimtimes > data/rf1.trig
 
-echo "Create human-readable ras file from binary output files "
-# (Assumes the auryn tool aube is accessible within your PATH)
-aube --input $OUTDIR/rf1.*.e.bras --from $T_START > $OUTDIR/rf1.e.ras
+echo "Create merged ras file from files from different ranks "
+sort -g -m $OUTDIR/rf1.*.e.ras > $OUTDIR/rf1.e.ras
 
 echo "Computing PSTH..."
 scripts/rassta.py -t data/rf1.trig -w 0.5 -f $OUTDIR/rf1.e.ras -s $T_START -m $T_STOP -q 0.0 -o data/rf1.sta
